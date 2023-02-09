72bcd52706b6c1044ea97b13b53b4b3444c44f6b
FuraSamura <78878137+FuraSamura@users.noreply.github.com>
2023-02-09 23:16:43 +0300

    1.0.2
    1.0.2

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,152 +1,255 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int R = 0;
+    public static String calc(String input) {
+        int c = 0;
+        String[] parts = input.split(" ");
+        String[] RomamN = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX"};
+        if (parts.length < 4) {
+            for (int i = 0; parts.length > i; i++) {
+                try {
+                    for (int j = 0; RomamN.length > j; j++) {
+
+                        if (parts[i].equals(RomamN[j])) {
+                            switch (parts[i]) {
+                                case ("I"):
+                                    R++;
+                                    parts[i] = "1";
+                                    break;
+                                case ("II"):
+                                    R++;
+                                    parts[i] = "2";
+                                    break;
+                                case ("III"):
+                                    R++;
+                                    parts[i] = "3";
+                                    break;
+                                case ("IV"):
+                                    R++;
+                                    parts[i] = "4";
+                                    break;
+                                case ("V"):
+                                    R++;
+                                    parts[i] = "5";
+                                    break;
+                                case ("VI"):
+                                    R++;
+                                    parts[i] = "6";
+                                    break;
+                                case ("VII"):
+                                    R++;
+                                    parts[i] = "7";
+                                    break;
+                                case ("VIII"):
+                                    R++;
+                                    parts[i] = "8";
+                                    break;
+                                case ("IX"):
+                                    R++;
+                                    parts[i] = "9";
+                                    break;
+                                case ("X"):
+                                    R++;
+                                    parts[i] = "10";
+                                    break;
+                                default:
+                                    System.out.println("Калькулятор принимает на вход числа от I до X");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex) {
+                    return "-1";
+                }
+            }
+            if (Integer.parseInt(parts[0]) > 10 || Integer.parseInt(parts[0]) < 1 || Integer.parseInt(parts[2]) > 10 || Integer.parseInt(parts[2]) < 1) {
+                System.out.println("Калькулятор принимает на вход числа от 1 до 10");
+            }
+            try {
+                switch (parts[1]) {
+                    case ("+"):
+                        c = Integer.parseInt(parts[0]) + Integer.parseInt(parts[2]);
+                        break;
+                    case ("-"):
+                        c = Integer.parseInt(parts[0]) - Integer.parseInt(parts[2]);
+                        break;
+                    case ("*"):
+                        c = Integer.parseInt(parts[0]) * Integer.parseInt(parts[2]);
+                        break;
+                    case ("/"):
+                        c = Integer.parseInt(parts[0]) / Integer.parseInt(parts[2]);
+                        break;
+                }
+                return Integer.toString(c);
+            }
+            catch (ArithmeticException ex) {
+                System.out.println("Невозможно поделить на 0");
+                return "-1";
+            }
+            catch (Exception ex) {
+                System.out.println("Неизвестная ошибка");
+                return "-1";
+            }
+        }
+        else {
+            System.out.println("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
+        return "-1";
+    }
+
+
     public static void main(String[] args) {
         while (true) {
-            int a, b;
-            int R = 0;
+            R = 0;
             System.out.println(" ");
             System.out.print("Напишите пример: ");
             try {
                 Scanner x = new Scanner(System.in);
                 String xx = x.nextLine();
-                String[] parts = xx.split(" ");
-                String[] RomamN = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX"};
-
-                for (int i = 0; parts.length > i; i++) {
-                    try {
-                        for (int j = 0; RomamN.length > j; j++) {
-
-                            if (parts[i].equals(RomamN[j])) {
-                                switch (parts[i]) {
-                                    case ("I"):
-                                        R++;
-                                        parts[i] = "1";
-                                        break;
-                                    case ("II"):
-                                        R++;
-                                        parts[i] = "2";
-                                        break;
-                                    case ("III"):
-                                        R++;
-                                        parts[i] = "3";
-                                        break;
-                                    case ("IV"):
-                                        R++;
-                                        parts[i] = "4";
-                                        break;
-                                    case ("V"):
-                                        R++;
-                                        parts[i] = "5";
-                                        break;
-                                    case ("VI"):
-                                        R++;
-                                        parts[i] = "6";
-                                        break;
-                                    case ("VII"):
-                                        R++;
-                                        parts[i] = "7";
-                                        break;
-                                    case ("VIII"):
-                                        R++;
-                                        parts[i] = "8";
-                                        break;
-                                    case ("IX"):
-                                        R++;
-                                        parts[i] = "9";
-                                        break;
-                                    case ("X"):
-                                        R++;
-                                        parts[i] = "10";
-                                        break;
-                                    default:
-                                        System.out.println("Калькулятор принимает на вход числа от I до X");
-                                }
-                            }
-                        }
-                    } catch (Exception ex) {
-
-                    }
-                }
                 if (!xx.contains(" ")) {
                     System.out.println("Вы ввели данные некорректно!");
                     break;
-                } else if (Integer.parseInt(parts[0]) > 10 || Integer.parseInt(parts[0]) < 1 || Integer.parseInt(parts[2]) > 10 || Integer.parseInt(parts[2]) < 1) {
-                    System.out.println("Калькулятор принимает на вход числа от 1 до 10");
-                } else {
-                    int j = Calculator.calculate(Integer.parseInt(parts[0]), Integer.parseInt(parts[2]), parts[1]);
+                }
+                else {
 
+                    int j = Integer.parseInt(calc(xx));
                     try {
                         if (R < 1) {
                             if (j != -1) {
                                 System.out.print(j);
                             }
-                        } else if (R > 1) {
-                            switch (j) {
-                                case (1):
-                                    System.out.println("I");
-                                    break;
-                                case (2):
-                                    System.out.println("II");
-                                    break;
-                                case (3):
-                                    System.out.println("III");
-                                    break;
-                                case (4):
-                                    System.out.println("IV");
-                                    break;
-                                case (5):
-                                    System.out.println("V");
-                                    break;
-                                case (6):
-                                    System.out.println("VI");
-                                    break;
-                                case (7):
-                                    System.out.println("VII");
-                                    break;
-                                case (8):
-                                    System.out.println("VIII");
-                                    break;
-                                case (9):
-                                    System.out.println("IX");
-                                    break;
-                                case (10):
-                                    System.out.println("X");
-                                    break;
-                                case (11):
-                                    System.out.println("XI");
-                                    break;
-                                case (12):
-                                    System.out.println("XII");
-                                    break;
-                                case (13):
-                                    System.out.println("XIII");
-                                    break;
-                                case (14):
-                                    System.out.println("XIV");
-                                    break;
-                                case (15):
-                                    System.out.println("XV");
-                                    break;
-                                case (16):
-                                    System.out.println("XVI");
-                                    break;
-                                case (17):
-                                    System.out.println("XVII");
-                                    break;
-                                case (18):
-                                    System.out.println("XVIII");
-                                    break;
-                                case (19):
-                                    System.out.println("XIX");
-                                    break;
-                                case (20):
-                                    System.out.println("XX");
-                                    break;
-                                default:
-                                    System.out.println("Калькулятор считает римские цифры от I до XX. Также не может быть минуса");
+                        }
+                        else if (R > 1)
+                        {
+                            int p = 0;
+                            String itog = "";
+                            if(j > 10 && j < 100)
+                            {
+                                if(j >= 40 && j < 50)
+                                {
+                                    p = j % 10;
+                                    itog = "XL";
+                                }
+                                if(j > 10 && j < 20)
+                                {
+                                    p = j % 10;
+                                    itog = "X";
+                                }
+                                if(j >= 20 && j < 30)
+                                {
+                                    p = j % 10;
+                                    itog = "XX";
+                                }
+                                if(j >= 30 && j < 40)
+                                {
+                                    p = j % 10;
+                                    itog = "XXX";
+                                }
+                                if(j >= 50 && j < 60)
+                                {
+                                    p = j % 10;
+                                    itog = "L";
+                                }
+                                if(j >= 60 && j < 70)
+                                {
+                                    p = j % 10;
+                                    itog = "LX";
+                                }
+                                if(j >= 70 && j < 80)
+                                {
+                                    p = j % 10;
+                                    itog = "LXX";
+                                }
+                                if(j >= 80 && j < 90)
+                                {
+                                    p = j % 10;
+                                    itog = "LXXX";
+                                }
+                                if(j >= 90 && j <= 99)
+                                {
+                                    p = j % 10;
+                                    itog = "XC";
+                                }
                             }
-                        } else {
+                            else if(j == 10)
+                            {
+                                itog += "X";
+                            }
+                            else if(j == 100)
+                            {
+                                itog += "C";
+                            }
+                            else if(j < 0 || j > 100) {
+                                System.out.println("Калькулятор считает римские цифры от I до C. Также не может быть минуса");
+                            }
+
+                            if(p != 0) {
+                                switch (p) {
+                                    case 1:
+                                        itog += "I";
+                                        break;
+                                    case 2:
+                                        itog += "II";
+                                        break;
+                                    case 3:
+                                        itog += "III";
+                                        break;
+                                    case 4:
+                                        itog += "IV";
+                                        break;
+                                    case 5:
+                                        itog += "V";
+                                        break;
+                                    case 6:
+                                        itog += "VI";
+                                        break;
+                                    case 7:
+                                        itog += "VII";
+                                        break;
+                                    case 8:
+                                        itog += "VIII";
+                                        break;
+                                    case 9:
+                                        itog += "IX";
+                                        break;
+                                }
+                            }
+                            else {
+                                switch (j) {
+                                    case 1:
+                                        itog += "I";
+                                        break;
+                                    case 2:
+                                        itog += "II";
+                                        break;
+                                    case 3:
+                                        itog += "III";
+                                        break;
+                                    case 4:
+                                        itog += "IV";
+                                        break;
+                                    case 5:
+                                        itog += "V";
+                                        break;
+                                    case 6:
+                                        itog += "VI";
+                                        break;
+                                    case 7:
+                                        itog += "VII";
+                                        break;
+                                    case 8:
+                                        itog += "VIII";
+                                        break;
+                                    case 9:
+                                        itog += "IX";
+                                        break;
+                                }
+                            }
+                            System.out.println(itog);
+                        }
+                        else
+                        {
                             System.out.println("Калькулятор умеет работать только с арабскими или римскими цифрами одновременно");
                             break;
                         }
@@ -163,38 +266,5 @@ public class Main {
             }
         }
     }
-}
-
-class Calculator {
-     public static int calculate(int a,int b, String sign){
-         int c = 0;
-         try
-         {
-             switch (sign) {
-                 case ("+"):
-                     c = a + b;
-                     break;
-                 case ("-"):
-                     c = a - b;
-                     break;
-                 case ("*"):
-                     c = a * b;
-                     break;
-                 case ("/"):
-                     c = a / b;
-                     break;
-                 }
-                 return c;
-         }
-         catch (ArithmeticException ex)
-         {
-             System.out.println("Невозможно поделить на 0");
-         }
-         catch (Exception ex)
-         {
-             System.out.println("Неизвестная ошибка");
-         }
-         return -1;
-     }
 }
 
